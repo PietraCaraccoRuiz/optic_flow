b27e5e4c2463c62487ceb6a8192557239b79f72f
PietraCaraccoRuiz <caraccoruizpietra@gmail.com>
2025-09-09 11:13:22 -0300

    fix team

--- src/sections/Team.jsx
+++ src/sections/Team.jsx
@@ -17,13 +17,13 @@ const Team = () => {
         <div className="absolute w-full h-[20vh] z-1 top-30 bg-gradient-to-t from-black/100 to-black/0"></div>
       </div>
       <div className="flex flex-col items-center justify-end px-6 text-center mb-[4vh] z-100">
-            <h1 className="text-3xl font-extrabold tracking-wide bg-gradient-to-b z-100 from-white to-gray-500/40 bg-clip-text text-transparent mt-[2vh]">
-              Development Team
-            </h1>
-            <p className="bg-gradient-to-l z-100 from-white to-gray-400/80 text-[1.2vh] mt-[1vh] text-justify bg-clip-text text-transparent z-2">
-              Alex Pinheiro, Antônio Rodrigues, Pedro Carreteiro, Pietra Ruiz, Samuel Miller
-            </p>
-          </div>
+        <h1 className="text-3xl font-extrabold tracking-wide bg-gradient-to-b z-100 from-white to-gray-500/40 bg-clip-text text-transparent mt-[2vh]">
+          Development Team
+        </h1>
+        <p className="bg-gradient-to-l z-100 from-white to-gray-400/80 text-[1.2vh] mt-[1vh] text-justify bg-clip-text text-transparent">
+          Alex Pinheiro, Antônio Rodrigues, Pedro Carreteiro, Pietra Ruiz, Samuel Miller
+        </p>
+      </div>
 
       <div className='relative w-full h-[35vh]'>
         <img className='absolute bottom-0' src={Grid} />
